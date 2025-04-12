41af7ec663f74538a13038224bdb7f9e96e66dae
semaples <semaples@my.waketech.edu>
2025-04-11 22:10:20 -0400

    Add files via upload

---
+++ gradebook.js
@@ -0,0 +1,17 @@
+// TODO fetch data from the postgreSQL database (to be implemented later)
+function fetchGradeData() {
+    // This function will query the postgreSQL database and return grade data
+    console.log("Fetching grade data...");
+}
+
+//TODO: Populate the table with grade data
+function populateGradebook(data) {
+    // This function will take the fetched grade data and populate the table 
+    console.log("populating gradebook with data", date);
+}
+
+//TODO REMOVE THIS 
+// Call the stubs to demonstrate the workflow
+const gradeData = fetchGradeData();
+populateGradebook(gradeData);
+//END REMOVE
